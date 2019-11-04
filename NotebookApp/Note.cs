0489cb699911baf0ea2f50881bf969fdfbccc0ab
Aleksandr <sashakamensk@mail.ru>
2019-11-05 01:14:31 +0300

    add app base

---
+++ NotebookApp/Note.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NotebookApp
+{
+    class Note
+    {
+        public string Name
+        {
+            get => Name;
+            set
+            {
+                while (value == "")
+                {
+                    Console.WriteLine("Поле не должно быть пустым!!!");
+                    value = Console.ReadLine();
+                }
+
+                Name = value;
+            }
+        }
+
+        public string Surname
+        {
+            get => Surname;
+            set
+            {
+                while (value == "")
+                {
+                    Console.WriteLine("Поле не должно быть пустым!!!");
+                    value = Console.ReadLine();
+                }
+
+                Surname = value;
+            }
+        }
+
+        public string Lastname
+        {
+            get => Lastname;
+            set
+            {
+                if (value == "") value = "Отсуствует";
+
+                Lastname = value;
+            }
+        }
+
+        public string PhoneNumber
+        {
+            get => PhoneNumber;
+            set
+            {
+                while (long.TryParse(value, out var number))
+                {
+                    Console.WriteLine("Номер состоит только из цифр!!");
+                    value = Console.ReadLine();
+                }
+
+                PhoneNumber = value;
+            }
+        }
+
+        public string Country
+        {
+            get => Country;
+            set
+            {
+                while (value == "")
+                {
+                    Console.WriteLine("Поле не должно быть пустым!!!");
+                    value = Console.ReadLine();
+                }
+
+                Country = value;
+            }
+        }
+
+        public DateTime? Birthdate
+        {
+            get => Birthdate;
+
+            set
+            {
+                if (value.ToString() == "") value = null;
+
+                Birthdate = value;
+            }
+        }
+
+        public string Organization
+        {
+            get => Organization;
+            set
+            {
+                if (value == "") value = "Отсуствует";
+
+                Organization = value;
+            }
+        }
+
+        public string Position
+        {
+            get => Position;
+            set
+            {
+                if (value == "") value = "Отсуствует";
+
+                Position = value;
+            }
+        }
+
+        public string OtherNotes
+        {
+            get => OtherNotes;
+            set
+            {
+                if (value == "") value = "Отсуствует";
+
+                OtherNotes = value;
+            }
+        }
+
+        public Note(DateTime? Birthday)
+        {
+            Birthdate = Birthday;
+        }
+    }
+}
---
+++ NotebookApp/Notebook.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+
+namespace NotebookApp
+{
+    class Notebook
+    {
+        private static List<Note> notes = new List<Note>();
+        static void Main(string[] args)
+        {
+            Companion.Greetings();
+            Companion.Info();
+            Start();
+        }
+
+        static void Start()
+        {
+            var isItEnd = true;
+            while (isItEnd)
+            {
+                var input = Console.ReadLine();
+                switch (input)
+                {
+                    case "1":
+                        CreateNote();
+                        break;
+                    case "2":
+                        Console.WriteLine("поиск");
+                        break;
+                    case "3":
+                        Console.WriteLine("ред");
+                        break;
+                    case "4":
+                        Console.WriteLine("удаление");
+                        break;
+                    case "5":
+                        ShowAllNotes();
+                        break;
+                    case "справка":
+                        Console.WriteLine("справка");
+                        break;
+                    case "пока":
+                        Console.WriteLine("Прощай, друг");
+                        Thread.Sleep(1000);
+                        isItEnd = false;
+                        break;
+                    default:
+                        Console.WriteLine("Ничего не понимаю! Введи корректную команду");
+                        break;
+                }
+                
+            }
+        }
+
+        static void CreateNote()
+        {
+            string a = "";
+            notes.Add(new Note(DateTime.Parse(a)));
+        }
+
+        static void ShowAllNotes()
+        {
+            if (notes.Count == 0) {
+                Console.WriteLine("На даный момент записей нет, но их же можно создать!");
+                return;
+            }
+            foreach (var item in notes)
+            {
+                Console.WriteLine($"Имя: {item.Name}");
+                Console.WriteLine($"Фамилия: {item.Surname}");
+                Console.WriteLine($"Телефон: {item.PhoneNumber}");
+                Console.WriteLine($"");
+            }
+        }
+
+        static void FindNote()
+        {
+            throw new NotImplementedException();
+        }
+
+        static void EditNote()
+        {
+            throw new NotImplementedException();
+        }
+
+        static void DeleteNote()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
