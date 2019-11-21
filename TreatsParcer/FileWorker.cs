ccb3306ebda6a2bc4f94d9272deafdee6c472767
Aleksandr <sashakamensk@mail.ru>
2019-11-21 16:20:54 +0300

    create lab2, add parsing and DataGrid

---
+++ TreatsParcer/FileWorker.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using OfficeOpenXml;
+
+
+namespace TreatsParcer
+{
+    class FileWorker
+    {
+        private  void Download()
+        {
+            using (var client = new WebClient())
+            {
+                client.DownloadFile("https://bdu.fstec.ru/documents/files/thrlist.xlsx", "data.xlsx");
+            }
+        }
+
+        public ArrayList CheckFile()
+        {
+            try
+            {
+                if (!File.Exists("data.xlsx"))  Download();
+                
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+            return Parse();
+        }
+
+        public ArrayList Parse()
+        {
+            var excelData = new ArrayList();
+            byte[] bin = File.ReadAllBytes("data.xlsx");
+            using (MemoryStream stream = new MemoryStream(bin))
+            using (ExcelPackage excelPackage = new ExcelPackage(stream))
+            {
+                int x = 0;
+                var sheet = excelPackage.Workbook.Worksheets[1];
+                //MessageBox.Show(sheet.Cells[3, 2].Value.ToString());
+                    //loop all rows
+                    for (int i = 3; i <= sheet.Dimension.End.Row; i++)
+                    {
+                        x++;
+                        var row = new string[8];
+                        for (int j = 1; j <= 8; j++)
+                        {
+                            var value = sheet.Cells[i, j].Value.ToString();
+                            row[j - 1] = value;
+                        }
+                        excelData.Add(new TreatInfo(row));
+                    }
+            }
+
+            return excelData;
+        }
+    }
+}
---
+++ TreatsParcer/TreatInfo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreatsParcer
+{
+        class TreatInfo
+        {
+            public string Id { get; }
+            public string Name { get; }
+            public string Description { get; }
+            public string Source { get; }
+            public string ExposureSubject { get; }
+            public bool IsHasPrivacyViolation { get; }
+            public bool IsHasIntegrityViolation { get; }
+            public bool IsHasAvailabilityViolation { get; }
+
+            public TreatInfo(string id, string name, string description, string source, string exposureSubject,
+                string isHasPrivacyViolation, string isHasIntegrityViolation, string isHasAvailabilityViolation)
+            {
+
+            }
+
+            public TreatInfo(string[] rowValues)
+            {
+                Id = rowValues[0];
+                Name = rowValues[1];
+                Description = rowValues[2];
+                Source = rowValues[3];
+                ExposureSubject = rowValues[4];
+                IsHasPrivacyViolation = rowValues[5].Equals("1");
+                IsHasIntegrityViolation = rowValues[6].Equals("1");
+                IsHasAvailabilityViolation = rowValues[7].Equals("1");
+            }
+        }
+
+   
+}
